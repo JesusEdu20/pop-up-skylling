4df4d6d28c22d98f4ecbf4dd788b7acbbabbed41
Jesus Eduardp <eduardoCastillo180@hotmail.com>
2023-03-27 04:21:44 -0400

    firts commit

---
+++ popup.js
@@ -0,0 +1,444 @@
+//OBJETO PARA ASIGNAR EVNTOS EN ELEMENTOS QUE DETONAN UNA ANIMACION DADA
+
+
+
+
+//LISTA DE TODOS LOS CODIGOS INTERNACIONALES TELEFONICOS DE LOS PAÍSES DEL MUNDO
+
+
+const countries = [  { name: 'Afganistán', code: '+93', bp:"-64px -16px;" },  { name: 'Albania', code: '+355', bp:"-112px -16px;" },  { name: 'Alemania', code: '+49'},  { name: 'Andorra', code: '+376' },  { name: 'Angola', code: '+244' },  { name: 'Anguila', code: '+1264' },  { name: 'Antigua y Barbuda', code: '+1268' },  { name: 'Arabia Saudita', code: '+966' },  { name: 'Argentina', code: '+54' },  { name: 'Armenia', code: '+374' },  { name: 'Aruba', code: '+297' },  { name: 'Australia', code: '+61' },  { name: 'Austria', code: '+43' },  { name: 'Azerbaiyán', code: '+994' },  { name: 'Bahamas', code: '+1242' },  { name: 'Bahréin', code: '+973' },  { name: 'Bangladés', code: '+880' },  { name: 'Barbados', code: '+1246' },  { name: 'Bélgica', code: '+32' },  
+{ name: 'Bermudas', code: '+1441' }, { name: 'Bielorrusia', code: '+375' }, { name: 'Birmania', code: '+95' }, { name: 'Bolivia', code: '+591' }, { name: 'Bosnia y Herzegovina', code: '+387' }, { name: 'Botsuana', code: '+267' }, { name: 'Brasil', code: '+55' }, { name: 'Brunéi', code: '+673' }, { name: 'Bulgaria', code: '+359' }, { name: 'Burkina Faso', code: '+226' }, { name: 'Burundi', code: '+257' }, { name: 'Bután', code: '+975' }, { name: 'Cabo Verde', code: '+238' }, { name: 'Camboya', code: '+855' }, { name: 'Camerún', code: '+237' }, { name: 'Canadá', code: '+1' }, { name: 'Chad', code: '+235' }, { name: 'Chile', code: '+56' }, { name: 'China', code: '+86' }, { name: 'Chipre', code: '+357' }, { name: 'Ciudad del Vaticano', code: '+379' }, { name: 'Colombia', code: '+57' }, { name: 'Comoras', code: '+269' }, { name: 'Corea del Norte', code: '+850' }, { name: 'Corea del Sur', code: '+82' }, { name: 'Costa de Marfil', code: '+225' }, { name: 'Costa Rica', code: '+506' }, { name: 'Croacia', code: '+385' }, { name: 'Cuba', code: '+53' }, { name: 'Curaçao', code: '+599' }, { name: 'Dinamarca', code: '+45' }, { name: 'Dominica', code: '+1767' }, { name: 'República Dominicana', code: '+1809' }, { name: 'Ecuador', code: '+593' }, { name: 'Egipto', code: '+20' }, { name: 'El Salvador', code: '+503' }, { name: 'Emiratos Árabes Unidos', code: '+971' }, { name: 'Eritrea', code: '+291' }, { name: 'Eslovaquia', code: '+421' }, { name: 'Eslovenia', code: '+386' }, { name: 'España', code: '+34' }, { name: 'Estados Unidos de América', code: '+1' }, { name: 'Estonia', code: '+372' }, { name: 'Etiopía', code: '+251' }, { name: 'Filipinas', code: '+63' }, { name: 'Finlandia', code: '+358' }, { name: 'Fiyi', code: '+679' }, { name: 'Francia', code: '+33' }, { name: 'Gabón', code: '+241' },
+{ name: 'Gambia', code : '+220' }, { name: 'Georgia', code: '+995' }, { name: 'Ghana', code: '+233' },
+{ name: 'Gibraltar', code: '+350' }, { name: 'Granada', code: '+1473' }, { name: 'Grecia', code: '+30' }, 
+{ name: 'Groenlandia', code: '+299' }, { name: 'Guam', code: '+1671' }, { name: 'Guatemala', code: '+502' },
+{ name: 'Guinea', code: '+224' }, { name: 'Guinea Ecuatorial', code: '+240' }, 
+{ name: 'Guinea-Bisáu', code: '+245' }, { name: 'Guyana', code: '+592' }, { name: 'Haití', code: '+509' },
+{ name: 'Honduras', code: '+504' }, { name: 'Hong Kong', code: '+852' }, { name: 'Hungría', code: '+36' }, 
+{ name: 'India', code: '+91' }, { name: 'Indonesia', code: '+62' }, { name: 'Irak', code: '+964' }, 
+{ name: 'Irán', code: '+98' }, { name: 'Irlanda', code: '+353' }, { name: 'Islandia', code: '+354' }, 
+{ name: 'Islas Cook', code: '+682' }, { name: 'Islas Feroe', code: '+298' }, 
+{ name: 'Islas Malvinas', code: '+500' }, { name: 'Islas Marshall', code: '+692' }, 
+{ name: 'Islas Salomón', code: '+677' }, { name: 'Israel', code: '+972' }, 
+{ name: 'Italia', code: '+39' }, { name: 'Jamaica', code: '+1876' }, { name: 'Japón', code: '+81' }, 
+{ name: 'Jordania', code: '+962' }, { name: 'Kazajistán', code: '+7' }, 
+{ name: 'Kenia', code: '+254' }, { name: 'Kirguistán', code: '+996' }, { name: 'Kiribati', code: '+686' }, { name: 'Kuwait', code: '+965' }, { name: 'Laos', code: '+856' }, { name: 'Lesoto', code: '+266' }, { name: 'Letonia', code: '+371' }, { name: 'Líbano', code: '+961' },{ name: 'Liberia', code: '+231' }, { name: 'Libia', code: '+218' }, { name: 'Liechtenstein', code: '+423' }, { name: 'Lituania', code: '+370' }, { name: 'Luxemburgo', code: '+352' }, { name: 'Macao', code: '+853' }, { name: 'Macedonia del Norte', code: '+389' }, { name: 'Madagascar', code: '+261' }, { name: 'Malasia', code: '+60' }, { name: 'Malaui', code: '+265' },{ name: 'Maldivas', code: '+960' }, { name: 'Malí', code: '+223' }, { name: 'Malta', code: '+356' }, { name: 'Marruecos', code: '+212' }, { name: 'Mauricio', code: '+230' }, { name: 'Mauritania', code: '+222' }, { name: 'México', code: '+52' }, { name: 'Micronesia', code: '+691' }, { name: 'Moldavia', code: '+373' }, { name: 'Mónaco', code: '+377' }, { name: 'Mongolia', code: '+976' }, { name: 'Montenegro', code: '+382' }, { name: 'Montserrat', code: '+1664' }, { name: 'Mozambique', code: '+258' }, { name: 'Myanmar (Birmania)', code: '+95' }, { name: 'Namibia', code: '+264' }, { name: 'Nauru', code: '+674' }, { name: 'Nepal', code: '+977' }, { name: 'Nicaragua', code: '+505' }, { name: 'Níger', code: '+227' }, { name: 'Nigeria', code: '+234' }, { name: 'Niue', code: '+683' }, { name: 'Noruega', code: '+47' }, { name: 'Nueva Caledonia', code: '+687' }, { name: 'Nueva Zelanda', code: '+64' }, { name: 'Omán', code: '+968' }, { name: 'Países Bajos (Holanda)', code: '+31' }, { name: 'Pakistán', code: '+92' }, { name: 'Palau', code: '+680' }, { name: 'Panamá', code: '+507' }, { name: 'Papúa Nueva Guinea', code: '+675' }, { name: 'Paraguay', code: '+595' }, { name: 'Perú', code: '+51' }, { name: 'Polinesia Francesa', code: '+689' }, { name: 'Polonia', code: '+48' }, { name: 'Portugal', code: '+351' }, { name: 'Puerto Rico', code: '+1787' }, { name: 'Qatar', code: '+974' }, { name: 'Reino Unido', code: '+44' }, { name: 'República Centroafricana', code: '+236' }, { name: 'República Checa', code: '+420' }, { name: 'República del Congo', code: '+242' }, { name: 'República Democrática del Congo', code: '+243' }, { name: 'República Dominicana', code: '+1809' }, { name: 'Reunión', code: '+262' }, { name: 'Ruanda', code: '+250' }, { name: 'Rumania', code: '+40' }, { name: 'Rusia', code: '+7' },
+{ name: 'Sáhara Occidental', code: '+212' }, { name: 'Samoa', code: '+685' },
+ { name: 'Samoa Americana', code: '+1684' }, { name: 'San Cristóbal y Nieves', code: '+1869' }, { name: 'San Marino', code: '+378' }, { name: 'San Vicente y las Granadinas', code: '+1784' }, { name: 'Santa Elena', code: '+290' }, { name: 'Santa Lucía', code: '+1758' }, { name: 'Santo Tomé y Príncipe', code: '+239' }, { name: 'Senegal', code: '+221' }, { name: 'Serbia', code: '+381' }, { name: 'Seychelles', code: '+248' }, { name: 'Sierra Leona', code: '+232' }, { name: 'Singapur', code: '+65' }, { name: 'Siria', code: '+963' }, { name: 'Somalia', code: '+252' }, { name: 'Sri Lanka', code: '+94' }, { name: 'Sudáfrica', code: '+27' }, { name: 'Sudán', code: '+249' },  { name: 'Suecia', code: '+46' }, { name: 'Suiza', code: '+41' }, { name: 'Surinam', code: '+597' }, { name: 'Swazilandia', code: '+268' }, { name: 'Tailandia', code: '+66' }, { name: 'Taiwán', code: '+886' }, { name: 'Tanzania', code: '+255' }, { name: 'Tayikistán', code: '+992' }, { name: 'Timor Oriental', code: '+670' }, { name: 'Togo', code: '+228' }, { name: 'Tokelau', code: '+690' }, { name: 'Tonga', code: '+676' }, { name: 'Trinidad y Tobago', code: '+1868' }, { name: 'Túnez', code: '+216' }, { name: 'Turkmenistán', code: '+993' }, { name: 'Turquía', code: '+90' }, { name: 'Tuvalu', code: '+688' }, { name: 'Ucrania', code: '+380' }, { name: 'Uganda', code: '+256' }, { name: 'Uruguay', code: '+598' }, { name: 'Uzbekistán', code: '+998' }, { name: 'Vanuatu', code: '+678' }, { name: 'Venezuela', code: '+58' }, { name: 'Vietnam', code: '+84' }, { name: 'Wallis y Futuna', code: '+681' }, { name: 'Yemen', code: '+967' }, { name: 'Yibuti', code: '+253' }, { name: 'Zambia', code: '+260' }, { name: 'Zimbabue', code: '+263' } 
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//INTERFAZ GRÁFICA DEL POP-UP
+
+
+/*La función setEventToCountries, configura un evento en el primer párametro siendo este un objeto de html, 
+cumpliendo con la rutina programada de obtener la clase de este elemento a través de (e.target.className)
+y posteriormente traer al elemento del DOM con  document.querySelector(`.${e.target.className}`),
+para obtener el valor del atributo codephone con targetElement.getAttribute("codephone") y de esta forma renderizar dicho valor en el elemento input suministrado en el segundo parámetro de la función
+
+Parámetros: 
+element: <li></li> elemento del dom (items de la lista) con el code numérico internacional   
+output: <input type=""text/> elemento del DOM, donde se rendizará el code numérico obtenido
+*/
+
+const setEventToCountries=(element, output)=>{
+
+    element.addEventListener("click", (e)=>{
+        
+        let targetElement=document.querySelector(`.${e.target.classList[0]}`); 
+          
+        output.value=`${targetElement.getAttribute("codephone")} `
+        /////////////////////////
+        /* colocar en una solo funcion */
+        if(phoneCodeButton.getAttribute("isClicked")){
+
+            listOfCountries.style.display="none"
+            phoneCodeButton.setAttribute("isClicked", "")
+
+            //activar animacion de la flecha del boton
+            animate(phoneCodeButtonArrow, {
+
+                out:[{transform:"rotate(90deg)"},{transform:"rotate(180deg)"}],
+                option:{duration:200, iterations:1, fill: "forwards"}
+    
+            }, true)
+
+            animate(phoneCodeButton, {
+
+                out:[{backgroundColor:"#003fd6"},{backgroundColor:"#FFFFFF"}],
+                option:{duration:200, iterations:1, fill: "forwards"}
+    
+            }, true)
+            
+        } 
+    })
+}
+
+/* Elementos del DOM */
+
+const listOfCountries=document.querySelector(".list-countries");
+const phoneCodeButton=document.querySelector(".phone-code-button");
+
+const phoneCodeInput= document.querySelector(".phone-code__input");
+const popupContainer= document.querySelector(".popup-container");
+const popup=document.querySelector(".popup");
+
+const phoneCodeButtonArrow=document.querySelector(".phone-code-button__arrow");
+
+const phoneCodeSend=document.querySelector(".phone-code__send");
+const spinner=document.querySelector(".spinner-config");
+
+const textSendButton=document.querySelector(".text-send");
+const phoneBackground=document.querySelector(".phone-background");
+
+const phoneBackgroundToMove=document.querySelector(".phone-background-to-move");
+
+
+/*FUNCIÓN: inserta todos los países (datos de la variable country) en la lista del DOM <ul></ul> */
+
+const insertCountries=()=>{
+    countries.map((country)=>{
+        //crear elemento html
+        var li = document.createElement('li');
+        const liContent=`${country.code} ${country.name}`
+        li.innerHTML=liContent
+
+        //insertar code como atributo del elemento
+
+        li.setAttribute("class", `flag-${country.name}`)
+        li.setAttribute("codePhone", country.code)
+
+        //algunos estilos para los elementos
+        li.style.cursor="pointer"
+
+        //insertar en lista
+        listOfCountries.appendChild(li)
+        setEventToCountries(li, phoneCodeInput);
+        
+    })
+}
+
+insertCountries()
+
+//Eventos
+
+phoneCodeButton.addEventListener("click", ()=>{
+
+    if(phoneCodeButton.getAttribute("isClicked")){
+
+        listOfCountries.style.display="none"
+        /*phoneCodeButtonArrow.style.transform="rotate(90deg)"*/
+        //ANIMACION DEL ICON DEL BUTTON
+        animate(phoneCodeButtonArrow, {
+
+            out:[{transform:"rotate(90deg)"},{transform:"rotate(180deg)"}],
+            option:{duration:200, iterations:1, fill: "forwards"}
+
+        }, true)
+
+        animate(phoneCodeButton, {
+
+            out:[{backgroundColor:"#003fd6"},{backgroundColor:"#FFFFFF"}],
+            option:{duration:200, iterations:1, fill: "forwards"}
+
+        }, true)
+
+        phoneCodeButton.setAttribute("isClicked", "")
+        
+    } 
+
+    else{
+
+        listOfCountries.style.display="block"
+        phoneCodeButton.setAttribute("isClicked", true)
+        
+
+        animate(phoneCodeButtonArrow, {
+
+            init:[{transform:"rotate(180deg)"},{transform:"rotate(90deg)"}],
+            option:{duration:200, iterations:1, fill: "forwards"}
+
+        }, false)
+
+        animate(phoneCodeButton, {
+
+            init:[{backgroundColor:"#FFFFFF"},{backgroundColor:"#003fd6"}],
+            option:{duration:200, iterations:1, fill: "forwards"}
+
+        }, false)
+    }
+})
+
+
+
+phoneCodeInput.addEventListener("keydown", (e)=>{
+
+    if((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105) && e.keyCode !==190  && e.keyCode !==110 && e.keyCode !==8 && e.keyCode !==9  ){
+        
+        phoneCodeInput.value=""
+        
+    }
+})
+
+/*Evento keydown para la animación e incorpoación del spinner perteneciente al boton send*/
+
+phoneCodeInput.addEventListener("keydown", (e)=>{
+    
+    //icon animado para el boton de send
+
+        /*//creando elemento html para el spinner(icon-animado) del botón send*/
+       
+        if(spinner.getAttribute("isPlayed") && phoneCodeInput.value.length>2){
+            
+            //botón para enviar el número bloqueado
+            phoneCodeInput.setAttribute("completed", "")
+
+            if(spinner.getAttribute("isPlayed") && phoneCodeInput.value.length>10 ){
+
+                textSendButton.style.display="inline-block"
+                //botón para enviar el número desbloqueados
+                phoneCodeInput.setAttribute("completed", "true")
+                spinner.style.display="none";
+               /* spinner.setAttribute("isPlayed", "");*/
+            }
+
+            else{
+                return
+            }
+          
+        } 
+
+        else if(spinner.getAttribute("isPlayed") && phoneCodeInput.value.length<2){
+
+            textSendButton.style.display="inline-block"
+               spinner.style.display="none";
+               spinner.setAttribute("isPlayed", "");
+      
+    
+            } 
+
+        else if(phoneCodeInput.value.length>1 && !spinner.getAttribute("isPlayed")){
+            
+            textSendButton.style.display="none"
+            textSendButton.innerHTML="Enviar"
+            spinner.style.display="inline-block";
+            spinner.setAttribute("isPlayed", "true");
+            animate(spinner, {
+                init:[{transform:"rotate(-360deg)"},{transform:"rotate(360deg)"}],
+                option:{duration:700, iterations:"Infinity", fill: "forwards"}
+            }, false)
+
+        }
+        
+})
+
+
+ 
+
+//phoneCodeSend
+
+phoneCodeInput.addEventListener('input',function(){
+    if (this.value.length > 12) 
+       this.value = this.value.slice(0,20); 
+    
+    /*if(this.value.length>=12){
+        popup.style.display="none"
+    }*/
+
+  })
+
+
+ 
+
+const sendButtonBackground=()=>{
+
+
+
+
+    textSendButton.addEventListener("mouseleave",()=>{
+
+    animate(phoneBackgroundToMove, {
+        out:[{transform:"translate(-100px) rotate(45deg)"}],
+        option:{duration:700, iterations:1, fill: "forwards"}
+    }, true)
+
+        phoneBackgroundToMove.setAttribute("isPlayed","")
+        
+  })
+
+    
+
+  textSendButton.addEventListener("mousemove",()=>{
+
+    
+    animate(phoneBackgroundToMove, {
+        init:[{transform:"translate(-400px) rotate(45deg)",}],
+        option:{duration:700, iterations:1, fill: "forwards"}
+    }, false)
+
+  })
+
+  phoneBackgroundToMove.setAttribute("isPlayed","true")
+
+}
+
+sendButtonBackground()
+   
+
+
+
+  /* ENVIAR PETICION AL SERVIDOR "POST" REGISTRAR NÚMERO */
+
+  textSendButton.addEventListener("click",()=>{
+
+
+    
+
+    if(phoneCodeInput.getAttribute("completed")){
+
+        fetch('https://', 
+        { method: 'POST', 
+        headers: { 'Content-Type': 'application/json' }, 
+        body: JSON.stringify({ number: phoneCodeInput.value}) 
+        }) 
+        .then(response => response.json()) 
+        .then(data => console.log(data)) 
+        .catch(error => console.error(error));
+
+        /*Animaciones para cierre del popup */
+
+        animate(popupContainer, {
+            
+            out:[{transform:"scale(1)"},{transform:"scale(0)"}],
+            option:{duration:200, iterations:1, fill: "forwards"}
+        }, true)
+
+        animate(popup, {
+            
+            out:[{backgroundColor:"#5f5f62f4", transform:"scale(1)", borderRadius:"80px"},{backgroundColor:"#5f5f6200", transform:"scale(0)", borderRadius:"80px"}],
+            option:{duration:200, iterations:1, fill: "forwards"}
+        }, true)
+
+        popup.setAttribute("isPop", "")
+        }
+        else{
+            textSendButton.innerHTML="Tu número"
+            animate(textSendButton, {
+            
+                init:[{transform:"scale(0)"},{transform:"scale(1)"}],
+                option:{duration:200, iterations:1, fill: "forwards"}
+            }, false)
+            
+
+        }
+
+
+  })
+
+  
+
+
+
+
+//ANIMACIONES
+
+const animate=(element, animation, played)=>{
+
+    if(played){
+     element.animate(
+         animation["out"],
+         animation["option"]
+     )
+    }
+    else{
+     element.animate(
+         animation["init"],
+         animation["option"]
+     )
+    }
+ 
+ }
+
+/*window.addEventListener("load",()=>{*/
+
+const pop=()=>{
+
+    if(popup.getAttribute("isPop")){
+
+        animate(popupContainer, {
+            init:[{transform:"scale(0)"},{transform:"scale(1)"}],
+            out:[{transform:"scale(1)"},{transform:"scale(0)"}],
+            option:{duration:400, iterations:1, fill: "forwards"}
+         }, true)
+    
+         animate(popup, {
+            init:[{backgroundColor:"#5f5f6200"},{backgroundColor:"#5f5f62f4"}],
+            out:[{backgroundColor:"#5f5f62f4"},{backgroundColor:"#5f5f6200"}],
+            option:{duration:500, iterations:1, fill: "forwards"}
+         }, true)
+
+         popup.setAttribute("isPop", "")
+    }
+
+    else{
+
+        animate(popupContainer, {
+            init:[{transform:"scale(0)"},{transform:"scale(1)"}],
+            out:[{transform:"scale(1)"},{transform:"scale(0)"}],
+            option:{duration:400, iterations:1, fill: "forwards"}
+         }, false)
+    
+         animate(popup, {
+            init:[{backgroundColor:"#5f5f6200"},{backgroundColor:"#5f5f62f4"}],
+            out:[{backgroundColor:"#5f5f62f4"},{backgroundColor:"#5f5f6200"}],
+            option:{duration:500, iterations:1, fill: "forwards"}
+         }, false)
+
+         popup.setAttribute("isPop", true)
+    }
+    
+}
+
+pop()
+/*})*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
